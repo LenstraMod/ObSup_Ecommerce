55f1762c3836ab6251eb6e0c7b97ce2a481bba02
Slugazoid <155078014+Slugazoid@users.noreply.github.com>
2025-05-22 23:17:46 +0700

    Update Payment.java

--- src/main/java/Payment/Payment.java
+++ src/main/java/Payment/Payment.java
@@ -1,5 +1,27 @@
 package Payment;
 
 public class Payment {
-
+  private String paymentID;
+	private Date paymentDate;
+	protected String paymentStatus;
+	protected int productAmount;
+	
+	public Payment(String paymentID, Date paymentDate, String paymentStatus, int productAmount) {
+		this.paymentID = paymentID;
+		this.paymentDate = paymentDate;
+		this.paymentStatus = paymentStatus;
+		this.productAmount = productAmount;
+	}
+	
+	public double calculatedPrice(double productPrice) {
+		System.out.println("Jumlah: " + productAmount);
+		return productPrice;
+	}
+	
+	public void paymentDetail(String productID, String userId) {
+		System.out.println("ID Pembayaran: " + paymentID);
+		System.out.println("Tanggal Pembayaran: " + paymentDate);
+		System.out.println("Status Pembayran: " + paymentStatus);
+		System.out.println("Jumlah Produk: " + productAmount);
+	}
 }
